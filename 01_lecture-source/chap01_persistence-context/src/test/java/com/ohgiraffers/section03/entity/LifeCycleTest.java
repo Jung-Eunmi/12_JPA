eb59ead611a90090aee80c6c4b727a47a0577dc5
DESKTOP-ET10H8L\hi <dmssl2486@gmail.com>
2025-01-07 15:54:53 +0900

    [250107]

--- 01_lecture-source/chap01_persistence-context/src/test/java/com/ohgiraffers/section03/entity/LifeCycleTest.java
+++ 01_lecture-source/chap01_persistence-context/src/test/java/com/ohgiraffers/section03/entity/LifeCycleTest.java
@@ -2,10 +2,12 @@ package com.ohgiraffers.section03.entity;
 
 import com.ohgiraffers.section01.entitymanager.EntityManagerGenerator;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class LifeCycleTest {
@@ -55,6 +57,94 @@ public class LifeCycleTest {
         Menu menu2 = manager.find(Menu.class, menuCode);
 
         Assertions.assertEquals(menu1, menu2);
+    }
+
+    /* comment.
+    *   엔티티 매니저가 영속성 컨텍스트에 엔티티 객체를 저장(persist)하게 된다면
+    *   영속성 컨텍스트가 관리할 수 있게되며, 이를 영속 상태라고 한다.
+    *   find(), creatQuery() 을 사용한 조회도 자동으로 영속 상태가 된다.
+    *   영속 상태인 엔티티 객체는 PK 로 조회를 하면 이미 관리되고 있기때문에 같은 객체(인스턴스)를 반환해준다. */
+
+    @ParameterizedTest
+    @DisplayName("준영속화 detach 테스트")
+    @CsvSource({"11, 1000", "12, 1000"})
+    void testDetachEntity(int menuCode, int menuPrice) {
+
+        EntityManager manager = EntityManagerGenerator.getInstance();
+
+        EntityTransaction transaction = manager.getTransaction();
+        // find() 로 영속관리가 되고있음
+        Menu foundMenu = manager.find(Menu.class, menuCode);
+
+        transaction.begin();
+        // detach : 특정 엔티티만 준영속 상태(관리하지 않는 상태)
+        manager.detach(foundMenu);
+        foundMenu.setMenuPrice(menuPrice);
+        manager.flush();
+
+        Assertions.assertEquals(menuPrice, manager.find(Menu.class, menuCode).getMenuPrice());
+
+        transaction.rollback();
+    }
+
+    @ParameterizedTest
+    @DisplayName("준영속화 detach 후 재영속화(marge) 테스트")
+    @CsvSource({"11, 1000", "12, 1000"})
+    void testDetachAndPersist(int menuCode, int menuPrice) {
+
+        EntityManager manager = EntityManagerGenerator.getInstance();
+
+        EntityTransaction transaction = manager.getTransaction();
+        // find() 로 영속관리가 되고있음
+        Menu foundMenu = manager.find(Menu.class, menuCode);
+
+        transaction.begin();
+        // detach : 특정 엔티티만 준영속 상태(관리하지 않는 상태)
+        manager.detach(foundMenu);
+        foundMenu.setMenuPrice(menuPrice);
+
+        // merge : 관리하지 않는 엔티티를 다시 관리 요청
+        manager.merge(foundMenu);
+
+        /* comment.
+        *   파라미터로 넘어 온 foundMenu 준영속 엔티티 객체의 식별자 값으로
+        *   1차 캐시에서 조회, 만약 1차 캐시에 엔티티가 없으면
+        *   DB 에서 엔티티 조회 후 1차 캐시에 저장한다.
+        *   조회 한 영속 엔티티 객체에 준영속 상태의 엔티티 객체의 값을 병합(merge)한 뒤
+        *   영속 엔티티 객체를 반환하게 된다. */
+        manager.flush();
+
+        Assertions.assertEquals(menuPrice, manager.find(Menu.class, menuCode).getMenuPrice());
+
+        transaction.rollback();
+    }
+
+    @ParameterizedTest
+    @DisplayName("영속성 엔티티 삭제(remove) 테스트")
+    @ValueSource(ints = {1})
+    void testRemoveEntity(int menuCode) {
+
+        EntityManager manager = EntityManagerGenerator.getInstance();
+
+        EntityTransaction transaction = manager.getTransaction();
+
+        Menu foundMenu = manager.find(Menu.class, menuCode);
+
+        transaction.begin();
+
+        /* comment.
+        *   remove() 엔티티를 영속성 컨텍스트 및 데이터베이스에서 제거한다.
+        *   단 트렌젝션을 제어하지 않으면 데이터베이스에 영구 반영되지는 않는다.
+        *   트렌젝션을 플러쉬, 커밋하는 순간 영속성 컨텍스트에서 관리하는 엔티티 객체를 DB 에 반영한다. */
+
+        manager.remove(foundMenu);
+        // remove 를 반영해주라고 flush
+        manager.flush();
+
+        // 같은 메뉴코드로 동일한 데이터 찾아지는 지 확인
+        Menu reFoundMenu = manager.find(Menu.class, menuCode);
+
+        Assertions.assertNull(reFoundMenu);
     }
 
 }
