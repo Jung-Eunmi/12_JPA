30d47115e2b29666fd4127ff4ad5a423eebafc92
DESKTOP-ET10H8L\hi <dmssl2486@gmail.com>
2025-01-08 11:15:27 +0900

    [250108]

---
+++ 01_lecture-source/chap02-mapping/src/main/java/com/ohgiraffers/mapping/section01/entity/MemberRepository.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.mapping.section01.entity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+/* comment.
+*   @Mapper DB Access 계층이 마이바티스 전용
+*   JPA 를 사용할 때는 @Repository 어노테이션을 사용하게 된다. */
+@Repository
+public class MemberRepository {
+
+    /* comment. Entity 매니저를 주입 받아 영속성 컨텍스트가 우리가 만든 Member 를 관리할 수 있도록 해준다. */
+    @PersistenceContext
+    private EntityManager manager;
+
+    public void save(Member member) {
+
+        // 매니저로 member 를 영속화 시키는 단계
+        manager.persist(member);
+
+
+    }
+}
---
+++ 01_lecture-source/chap02-mapping/src/main/java/com/ohgiraffers/mapping/section01/entity/MemberRole.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.mapping.section01.entity;
+
+// enum : 미리 정의된 상수 값들의 집합 (변하지 않는 정보일 때 사용)
+public enum MemberRole {
+
+    ROLE_MEMBER, ROLE_ADMIN
+}
